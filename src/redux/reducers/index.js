655aa2e79c0e01ecf2edecea885047ae1b24acc2
Riccardo <richi-97@live.it>
2025-12-03 14:25:37 +0100

    added icons pack and settings for store and reducer

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,16 @@
+const initialState = {
+  fav: {
+    content: [],
+  },
+};
+
+const mainReducer = function (currState = initialState, action) {
+  switch (action.type) {
+    // case
+    // case
+    default:
+      return currState;
+  }
+};
+
+export default mainReducer;
